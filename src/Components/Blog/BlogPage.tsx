c0908d0feeb9bf9c4b2b9c9913d00d1dde094da8
Ibeneme <ibenemeikenna96@gmail.com>
2025-02-13 14:43:58 +0100

    updates

--- src/Components/Blog/BlogPage.tsx
+++ src/Components/Blog/BlogPage.tsx
@@ -20,7 +20,7 @@ const BlogPage: React.FC = () => {
   }, [dispatch]);
 
   const handleClick = (id: string) => {
-    navigate(`/blog-details/${id}`); // Navigate to the blog details page by ID
+    navigate(`/blog-details/${id}`);
   };
 
   const truncateText = (text: string, maxLength: number = 200) => {
@@ -40,31 +40,34 @@ const BlogPage: React.FC = () => {
             {status === "loading" && <p>Loading...</p>}
             {status === "failed" && <p>Error: {error}</p>}
             {status === "succeeded" &&
-              insights.map((post) => (
-                <div
-                  key={post._id}
-                  className="blog-card"
-                  onClick={() => handleClick(post._id)} // Pass the ID instead of the whole post
-                >
-                  <img
-                    src={post.image}
-                    alt={post.title}
-                    className="blog-image"
-                  />
-                  <div className="blog-card-content">
-                    <h2>{post.title}</h2>
-                    <p>{truncateText(post.content)}</p>
-                    <p className="blog-date">
-                      {post.dateCreated
-                        ? formatDate(new Date(post.dateCreated))
-                        : "Unknown date"}
-                    </p>
+              insights
+                ?.slice()
+                ?.reverse()
+                ?.map((post) => (
+                  <div
+                    key={post._id}
+                    className="blog-card"
+                    onClick={() => handleClick(post?._id)} // Pass the ID instead of the whole post
+                  >
+                    <img
+                      src={post?.image}
+                      alt={post?.title}
+                      className="blog-image"
+                    />
+                    <div className="blog-card-content">
+                      <h2>{post?.title}</h2>
+                      <p>{truncateText(post?.content)}</p>
+                      <p className="blog-date">
+                        {post?.dateCreated
+                          ? formatDate(new Date(post?.dateCreated))
+                          : "Unknown date"}
+                      </p>
+                    </div>
+                    <div className="blog-image-overlay">
+                      <FaArrowRight className="arrow-icon" />
+                    </div>
                   </div>
-                  <div className="blog-image-overlay">
-                    <FaArrowRight className="arrow-icon" />
-                  </div>
-                </div>
-              ))}
+                ))}
           </div>
         </div>
       </div>
--- src/Components/LegalInsights/LegalInsights.tsx
+++ src/Components/LegalInsights/LegalInsights.tsx
@@ -90,57 +90,60 @@ const LegalInsights: React.FC = () => {
       <br />
       <div style={{ padding: "150px 0px", backgroundColor: "#f4f4f4" }}>
         <div className="legal-insights-grid">
-          {insights?.map((insight) => (
-            <div
-              onClick={() => handleReadMore(insight)}
-              key={insight?._id}
-              style={{ display: "flex", flexDirection: "column" }}
-            >
+          {insights
+            ?.slice()
+            ?.reverse()
+            ?.map((insight) => (
               <div
-                className="legal-insight-item"
-                style={{
-                  backgroundImage: `url(${insight?.image || fozaImage})`,
-                  borderRadius: 32,
-                }}
-              ></div>
-
-              <div
-                style={{
-                  padding: 16,
-                  backgroundColor: "#fff",
-                  borderRadius: 32,
-                  marginTop: 8,
-                }}
+                onClick={() => handleReadMore(insight)}
+                key={insight?._id}
+                style={{ display: "flex", flexDirection: "column" }}
               >
-                <h1 style={{ textAlign: "left", fontSize: 24 }}>
-                  {insight?.title}
-                </h1>
-                <p>
-                  {formatDescription(insight?.description?.slice(0, 180))}
-                  {formatDescription(
-                    insight?.description?.length > 180 ? "..." : ""
-                  )}
-                </p>
-                <p>
-                  <strong>Created:</strong> {formatDate(insight?.dateCreated)}
-                </p>
-
-                <button
-                  onClick={() => handleReadMore(insight)}
+                <div
+                  className="legal-insight-item"
                   style={{
-                    backgroundColor: "transparent",
-                    color: "green",
-                    padding: 0,
-                    margin: 0,
-                    marginBottom: 24,
-                    marginTop: 16,
+                    backgroundImage: `url(${insight?.image || fozaImage})`,
+                    borderRadius: 32,
+                  }}
+                ></div>
+
+                <div
+                  style={{
+                    padding: 16,
+                    backgroundColor: "#fff",
+                    borderRadius: 32,
+                    marginTop: 8,
                   }}
                 >
-                  Read More <FaChevronRight />
-                </button>
+                  <h1 style={{ textAlign: "left", fontSize: 24 }}>
+                    {insight?.title}
+                  </h1>
+                  <p>
+                    {formatDescription(insight?.description?.slice(0, 180))}
+                    {formatDescription(
+                      insight?.description?.length > 180 ? "..." : ""
+                    )}
+                  </p>
+                  <p>
+                    <strong>Created:</strong> {formatDate(insight?.dateCreated)}
+                  </p>
+
+                  <button
+                    onClick={() => handleReadMore(insight)}
+                    style={{
+                      backgroundColor: "transparent",
+                      color: "green",
+                      padding: 0,
+                      margin: 0,
+                      marginBottom: 24,
+                      marginTop: 16,
+                    }}
+                  >
+                    Read More <FaChevronRight />
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </div>
